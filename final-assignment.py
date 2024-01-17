6d9a39425415ef86296f3680c52952543c17219a
pabloth1 <156604981+pabloth1@users.noreply.github.com>
2024-01-17 12:45:16 +0100

    Added documentation for the load_and_clean_data function in final-assignment.py

--- final-assignment.py
+++ final-assignment.py
@@ -1,4 +1,7 @@
 from data_cleaning_formatting import load_and_clean_data
 
+# Loading and Cleaning Australian Domestic Flight
+# The 'load_and_clean_data' function is invoked with the path to a CSV file containing australian domestic flight data.
+# This function returns a cleaned pandas DataFrame, stored in the "data" variable, ready for analysis and visualization.
 data = load_and_clean_data('dom_city_pair.csv')
-#testing
+
